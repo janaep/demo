68e8b93bb7a87f1b29da67266592aa284602fc38
janaep <janaeperez117@gmail.com>
2023-09-04 23:00:22 -0400

    add1

---
+++ client/src/Post.js
@@ -0,0 +1,57 @@
+export default function Post(){
+    return (
+     <>
+    <div className= "post">
+      <div className= "image">
+        <img src = "https://blog.logomyway.com/wp-content/uploads/2022/03/barbie-logo.png" alt = "" width = "400" height = "500" />
+      </div>
+
+      <div className = "texts">
+         <h2>Who is the inspiration behind the famous Barbie doll?</h2>
+         <p className= "info">
+          <a className= "author"> Unknown </a>
+          <time>2023-08-27 14:58</time>
+         </p>
+         <p className = "summary"> 'The buzz behind Greta Gerwig's "Barbie” movie is undeniable – but this is hardly the first time the country has been swept by Barbiemania.' </p>
+      </div>
+    
+    </div>
+    
+    <div className= "post">
+      <div className= "image">
+        <img src = "https://variety.com/wp-content/uploads/2023/04/Barbie-Movie.jpg" alt = "" width = "650" height = "500" />
+      </div>
+
+      <div className = "texts">
+          <h2>Why Does Margot Robbie Wear a Yellow Dress at the End of ‘Barbie’?</h2>
+          
+          <p className= "info">
+            <a className= "author"> Unknown </a>
+           <time>2023-08-27 14:58</time>
+         </p>
+
+          <p className ="summary"> The yellow dress Margot Robbie’s wears in Barbie’s final scenes was designed to represent change, says the film’s costume designer Jacqueline Durran. </p>
+      </div>
+
+   
+    </div>
+
+    <div className= "post">
+      <div className= "image">
+        <img src = "https://img.apmcdn.org/924d9e2e314c5b5be9a748254f07fc376b88fef8/uncropped/4d952d-20230718-barbie-doll-17-600.jpg" alt = "" width = "" height = "500" />
+      </div>
+      <div className = "texts">
+          <h2>In 1963, she left Minneapolis for Mattel. She designed Barbie clothes for 35 years</h2>
+
+         <p className= "info">
+           <a className= "author"> Unknown </a>
+            <time>2023-08-27 14:58</time>
+         </p>
+
+          <p className= "summary"> Carol Spencer began working as a designer for Mattel in 1963, where she spent 35 years designing clothes for Barbie dolls before retiring in 1998. Now 90 years old, she has hundreds of dolls whose outfits she designed that she is slowly selling off. </p>
+      </div>
+
+    </div>
+    </>
+    );
+}
